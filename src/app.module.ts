4d78d177f306093b331255cc27a41bdf6bd21b9f
matheus-cavalini <matheuscavaliniti@gmail.com>
2024-07-28 12:11:32 -0300

    creating product module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
     }),
-    MongooseModule.forRoot(process.env.DATABASE_URI)
+    MongooseModule.forRoot(process.env.DATABASE_URI),
+    ProductsModule
   ],
   controllers: [AppController],
   providers: [AppService],
